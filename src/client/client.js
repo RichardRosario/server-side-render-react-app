f107327e2c1fb6c1c244f87dfffe84daba411620
unknown <tech2js@gmail.com>
2018-08-07 16:04:56 +0800

    added actions and reducers and helpers

--- src/client/client.js
+++ src/client/client.js
@@ -1,12 +1,20 @@
-//start up point for client side app
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import Routes from './Routes';
 import { BrowserRouter } from 'react-router-dom';
+import reducers from './reducers'; 
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
-<BrowserRouter>
-  <Routes/>
-</BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
+</Provider>,
 
 document.querySelector('#root'));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 
 const app = express();
@@ -7,8 +9,11 @@ const app = express();
 app.use(express.static('public'));
 //route handler
 app.get('*', (req, res) => {
+  const store = createStore();
+
+  //logic to initial and load data into store
   
-  res.send(renderer(req));
+  res.send(renderer(req, store));
 });
 
 app.listen( 3000, () => {
